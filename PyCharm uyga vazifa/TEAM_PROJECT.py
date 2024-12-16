9c5b082c03c5ea181e13b205dcf8c92ac2caf708
Ziyo6ka <shamsiddinovziyovuddin2010@gmail.com>
2024-12-16 19:46:08 +0500

    projekt_work

--- PyCharm uyga vazifa/TEAM_PROJECT.py
+++ PyCharm uyga vazifa/TEAM_PROJECT.py
@@ -1,9 +1,9 @@
 #Magazin aftamatlashtiradigan
-class User:
+#10ta mahsulot 4ta user 1tta admin
 #name,is_admin=admin,is_client=client,karzinka =[],balans=10000,password = password
-class Mahsulotlar:
+# class Mahsulotlar:
 #nomi,narxi,muddati
-
-class Dukon:
+# class Dukon:
 #title(nom),baza(mahsulotlar baza),balans,history(bo'sh massiv),umumiy-qaysi user qancha foyda qigani
 #4ta user 1ta admin
+
