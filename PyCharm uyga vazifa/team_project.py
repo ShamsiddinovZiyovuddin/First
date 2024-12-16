6f3b86ca4f6905045dbaaa469a0438d8e8c029f0
Ziyo6ka <shamsiddinovziyovuddin2010@gmail.com>
2024-12-16 21:03:44 +0500

    dars

--- PyCharm uyga vazifa/team_project.py
+++ PyCharm uyga vazifa/team_project.py
@@ -8,10 +8,15 @@ from project.class_User import user1
 
 
 def Shop():
+    while True:
+        print("No  Mahsulot nomi  Narxi (so'm)  Muddati")
+        for i in range(1, len(goods)):
+            print(f"{i}".ljust(3), f"{goods[i].name}".ljust(14), f"{goods[i].cost}".ljust(13),
+                  f"{(goods[i]).date[0]}.{goods[i].date[1]}.{goods[i].date[2]}")
 
-    status=input("qaysi mahsulotni sotib olmoqchisiz:")
-    if status=="1":
-        status2=input("qancha sotib olasiz:")
-        narx=status2*good1.cost
-        user.karzinka+=f"{status2}ta {good1}"
-    elif status=="2":
+        status=input("qaysi mahsulotni sotib olmoqchisiz:")
+        if status=="1":
+            status2=input("qancha sotib olasiz:")
+            narx=status2*good1.cost
+            user.karzinka+=f"{status2}ta {good1}"
+        elif status=="2":
