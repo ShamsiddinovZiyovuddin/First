750d863d24b903308c4a5439b9b6acaaee8957f4
Ziyo6ka <shamsiddinovziyovuddin2010@gmail.com>
2024-12-16 21:07:46 +0500

    dars

--- PyCharm uyga vazifa/team_project.py
+++ PyCharm uyga vazifa/team_project.py
@@ -2,13 +2,18 @@ from collections import UserDict
 
 from class_Goods import goods
 from class_Shop import supermarkets
-from class_User import User
+from class_User import users
 from project.class_Goods import good1
 from project.class_User import user1
 
 
 def Shop():
     while True:
+        client_phone = input("Kliyentning telefon raqami : ")
+        for client in users:
+            if int(client_phone) == client.phone:
+                new_client = client
+
         print("No  Mahsulot nomi  Narxi (so'm)  Muddati")
         for i in range(1, len(goods)):
             print(f"{i}".ljust(3), f"{goods[i].name}".ljust(14), f"{goods[i].cost}".ljust(13),
@@ -18,5 +23,5 @@ def Shop():
         if status=="1":
             status2=input("qancha sotib olasiz:")
             narx=status2*good1.cost
-            user.karzinka+=f"{status2}ta {good1}"
+            new_client.karzinka+=f"{status2}ta {good1}"
         elif status=="2":
